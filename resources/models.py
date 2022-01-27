93d2e60508f1949ae5db607486e682bcd267d083
James Echmalian <ech@ech.net>
2022-01-27 16:45:28 -0500

    Resource model: add type field, add doc.

--- resources/models.py
+++ resources/models.py
@@ -5,22 +5,73 @@ from django.utils.translation import gettext_lazy as _
 from boards.models import BoardScopedModelMixin
 
 
+class ResourceType():
+  PERSON = 1
+  EQUIPMENT = 2
+  VEHICLE = 3
+  CHOICES = (
+      (PERSON, "Person"),
+      (EQUIPMENT, "Equipment"),
+      (VEHICLE, "Vehicle"),
+  )
+
+
 class Resource(BoardScopedModelMixin):
 
   class Meta:
     verbose_name = "resource"
 
-  image_url = models.URLField(blank=True, null=True, max_length=200)
+  # A resource may be a person, a piece of equipment, or a vehicle.
+  type = models.PositiveIntegerField(
+      choices=ResourceType.CHOICES,
+      null=False,
+      blank=False,
+      verbose_name=_("resource type"),
+  )
 
-  display_name = models.TextField(blank=False, null=False, db_index=True)
+  # An optional icon or avatar.
+  image_url = models.URLField(
+      blank=True,
+      null=True,
+      max_length=200,
+      verbose_name=_("image url"),
+  )
 
-  description = models.TextField(blank=True, null=True)
+  # The name to appear in the Resource Grid.
+  display_name = models.TextField(
+      blank=False,
+      null=False,
+      db_index=True,
+      verbose_name=_("display name"),
+  )
 
-  # A Resource may be associated with a User.
-  user = models.ForeignKey(to=get_user_model(), blank=True, null=True, on_delete=models.SET_NULL)
+  # The name to appear in the Resource Grid.
+  display_name = models.TextField(
+      blank=False,
+      null=False,
+      db_index=True,
+      verbose_name=_("display name"),
+  )
 
+  # Descriptive text.
+  description = models.TextField(
+      blank=True,
+      null=True,
+      verbose_name=_("description"),
+  )
+
+  # A Resource may be associated with a User.  (However, it is not required that every person have a user.)
+  user = models.ForeignKey(
+      to=get_user_model(),
+      blank=True,
+      null=True,
+      on_delete=models.SET_NULL,
+      verbose_name=_("user"),
+  )
+
+  # TODO: this is what serializers are for.
   def to_json(self):
     return {
         key: getattr(self, key)
-        for key in ("id", "image_url", "display_name", "description", "user_id")
+        for key in ("id", "type", "image_url", "display_name", "description", "user_id")
     }
